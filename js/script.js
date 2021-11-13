5eb9725f6dcc1410d9a896930ff76ba771bf5476
rafaelpinheiro84 <rafaelsp0584@gmail.com>
2021-11-13 19:34:12 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//Menu de Barras com JS
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+let email = document.getElementById("campo-email");
+let mensagem = document.getElementById("campo-mensagem");
+
+////window.alert("teste");
+function mostrarPopup() {
+    window.alert("Hello World")
+}
+
+//DOM- Document Object Model
+// regex -expressoes regulares
+
+function enviarEmail() {
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
+
+function enviarMensagem() {
+    let mensagemDigitada = mensagem.value
+    console.log(mensagemDigitada)
+}
